d1a0c55f971a3b8f29889e7efc4984958e012dd9
Vesna Tan <tanvezi@gmail.com>
2018-12-18 10:25:03 -0500

    added event headers

--- handler.js
+++ handler.js
@@ -1,14 +1,15 @@
-"use strict"
+"use strict";
 
 exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Go Server v1.0! Your function executed successfully!"
-      // input: event
+      message: "Go Server v1.0! Your function executed successfully!",
+      input: event,
+      headers: event.headers["x-api-key"]
     })
-  }
+  };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-}
+};
