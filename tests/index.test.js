4b888f6b56fa28daef6aac1713411d84a1033208
Vesna Tan <tanvezi@gmail.com>
2019-02-15 10:13:03 -0500

    changed paths node_modules

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,5 +3,5 @@ const handler = require("../index").hello;
 const { expect } = chai;
 it("returns 200", async () => {
   const res = await handler("test", null, (err, result) => result);
-  expect(res.statusCode).to.equal(400);
+  expect(res.statusCode).to.equal(200);
 });
